4a139575b046417b0c813118ee208cd07deb2b07
meant-ion <popepontus@tutanota.com>
2022-06-18 10:14:20 -0500

    Fixed commit

--- src/audio/audio.py
+++ src/audio/audio.py
@@ -2,7 +2,7 @@ from playsound import playsound
 import os, sys, random
 
 def main(index):
-    sound_list = ['shotgunfiring.mp3','bonk.mp3','reeee.mp3','lotofdmg.mp3','airhorn.mp3',
+    sound_list = ['shotgunfiring.mp3','bonk.mp3','reeee.mp3','airhorn.mp3',
                   'fail.mp3','notfine.mp3','animegod.mp3','rack.mp3',
                   'nogodplsno.mp3','tactinuke.mp3','bababooey.mp3','knockknock.mp3']
     playsound(os.path.join(sys.path[0], sound_list[index]))
@@ -13,4 +13,4 @@ if __name__ == "__main__":
         i = int(val[0])
         main(i)
     else:
-        main(random.randint(0,11))
+        main(random.randint(0,10))
